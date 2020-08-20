7b15dc8ad80830aa022e15664a5caf6e6c538bde
gyektai <gideonyektai@gmail.com>
2020-08-19 21:07:43 -0400

    Start to create the blog post functionality. Make forms file, write template, and call from mysite

--- mysite.py
+++ mysite.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from forms import PostForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '0c702f92f9d7843302720201f994eb10'
 
 @app.route('/home')
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title='GY for Gap Year')
+
+@app.route('/makepost')
+def makepost():
+	form = PostForm()
+	return render_template('write.html', title='Make Post', form=form)
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
